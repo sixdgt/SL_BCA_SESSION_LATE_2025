1b2ba8b1b0e68591972c4a2beaa7118700ed3402
sixdgt <c4crypt@gmail.com>
2025-08-07 09:56:43 +0545

    SL: JavaScript Day 1 - git demo

---
+++ day_1/script.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log("Trying git and github for the first time");
+console.log("Repo created & ssh-key generated");
+console.log("Repo cloned & code added");
+console.log("Code committed & pushed");
+console.log("Code pulled & merged");
+console.log("Code pushed & pulled");
+console.log("Code pulled & merged");
